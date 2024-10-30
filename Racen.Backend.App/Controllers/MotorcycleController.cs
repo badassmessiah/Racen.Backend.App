65200642a7eaf5965ec8ba935cbb99ce8bab4794
badassmessiah <lashaandguladze69@gmail.com>
2024-10-30 14:54:47 +0400

    Add MotorcycleController and migration for Enabled property in Motorcycles and Items

---
+++ Racen.Backend.App/Controllers/MotorcycleController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Racen.Backend.App.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MotorcycleController : ControllerBase
+    {
+        
+    }
+}
---
+++ Racen.Backend.App/Migrations/20241030105058_CreateMotorcycleAndItemsServices.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Racen.Backend.App.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateMotorcycleAndItemsServices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Enabled",
+                table: "Motorcycles",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Enabled",
+                table: "Items",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Enabled",
+                table: "Motorcycles");
+
+            migrationBuilder.DropColumn(
+                name: "Enabled",
+                table: "Items");
+        }
+    }
+}
